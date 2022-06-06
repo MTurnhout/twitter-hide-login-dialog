86148564228cf4a3fb43d090cfa1f350836721c2
MTurnhout <MTurnhout@users.noreply.github.com>
2022-06-06 14:14:25 +0200

    Move functionality to class

--- src/extension.js
+++ src/extension.js
@@ -1,74 +1,96 @@
-const loginUrlRegEx = /^\/(?:i\/|login).*/;
-const observerLayers = new MutationObserver(onLayersMutations);
-let observerParent;
-let layersElement;
-
-window.addEventListener('load', checkUntilLayersExist);
-
 /**
- * Wait till React application is done loading and
- * start observing layers elements and parent.
+ * Twitter hide login dialog add-on functionality.
  */
-function checkUntilLayersExist() {
-    const checkExistInterval = setInterval(() => {
-        layersElement = document.getElementById("layers");
-        if (layersElement) {
-            clearInterval(checkExistInterval);
+class TwitterHideLoginDialogAddon {
+    static #loginUrlRegEx = /^\/(?:i\/|login).*/;
+    #observerLayers;
+    #observerParent;
+    #layersElement;
 
-            startObservingLayersAndParent();
-        }
-    }, 100);
-}
+    /**
+     * Hide login dialog on appropriate pages.
+     */
+    hide() {
+        this.#checkUntilLayersExist();
+    }
 
-/**
- * Start observing layers elements and parent.
- */
-function startObservingLayersAndParent() {
-    observerLayers.observe(layersElement, { childList: true });
+    /**
+     * Wait till React application is done loading and
+     * start observing layers elements and parent.
+     */
+    #checkUntilLayersExist() {
+        const checkExistInterval = setInterval(() => {
+            this.#layersElement = document.getElementById("layers");
+            if (this.#layersElement) {
+                clearInterval(checkExistInterval);
+
+                this.#startObservingLayersAndParent();
+            }
+        }, 100);
+    }
     
-    if (!observerParent) {
-        observerParent = new MutationObserver(onLayersParentMutations);
-        observerParent.observe(layersElement.parentNode, { childList: true });
-    }
-}
-
-/**
- * Monitor if login dialog is added and hide on appropriate page.
- * @param {MutationRecord[]} mutations 
- */
-function onLayersMutations(mutations) {
-    if (loginUrlRegEx.test(window.location.pathname))
-        return;
-
-    for (const mutation of mutations) {
-        for (const addedNode of mutation.addedNodes) {
-            const isLoginDialog = document.evaluate(
-                ".//div[@role='dialog']//span[text()='Log in']",
-                addedNode,
-                null,
-                XPathResult.BOOLEAN_TYPE,
-                null).booleanValue;
+    /**
+     * Start observing layers elements and parent.
+     */
+    #startObservingLayersAndParent() {
+        if (!this.#observerLayers) {
+            this.#observerLayers = new MutationObserver((mutations) => {
+                this.#onLayersMutations(mutations);
+            });
+        }
+        this.#observerLayers.observe(this.#layersElement, { childList: true });
         
-            if (isLoginDialog) {
-                addedNode.style.display = "none";
-                
-                document.documentElement.style.overflow = "auto";
+        if (!this.#observerParent) {
+            this.#observerParent = new MutationObserver((mutations) => {
+                this.#onLayersParentMutations(mutations);
+            });
+            this.#observerParent.observe(this.#layersElement.parentNode, { childList: true });
+        }
+    }
+
+    /**
+     * Monitor if login dialog is added and hide on appropriate pages.
+     * @param {MutationRecord[]} mutations
+     */
+    #onLayersMutations(mutations) {
+        if (TwitterHideLoginDialogAddon.#loginUrlRegEx.test(window.location.pathname))
+            return;
+
+        for (const mutation of mutations) {
+            for (const addedNode of mutation.addedNodes) {
+                const isLoginDialog = document.evaluate(
+                    ".//div[@role='dialog']//span[text()='Log in']",
+                    addedNode,
+                    null,
+                    XPathResult.BOOLEAN_TYPE,
+                    null).booleanValue;
+            
+                if (isLoginDialog) {
+                    addedNode.style.display = "none";
+                    
+                    document.documentElement.style.overflow = "auto";
+                }
+            }
+        }
+    }
+    
+    /**
+     * Monitor if layers element is removed and observe new layers element.
+     * @param {MutationRecord[]} mutations
+     */
+    #onLayersParentMutations(mutations) {
+        for (const mutation of mutations) {
+            for (const removedNode of mutation.removedNodes) {
+                if (removedNode === this.#layersElement) {
+                    this.#observerLayers.disconnect();
+                    this.#checkUntilLayersExist();
+                }
             }
         }
     }
 }
 
-/**
- * Monitor if layers element is removed and observe new layers element.
- * @param {MutationRecord[]} mutations 
- */
-function onLayersParentMutations(mutations) {
-    for (const mutation of mutations) {
-        for (const removedNode of mutation.removedNodes) {
-            if (removedNode === layersElement) {
-                observerLayers.disconnect();
-                checkUntilLayersExist();
-            }
-        }
-    }
-}
+const twitterHideLoginDialogAddon = new TwitterHideLoginDialogAddon();
+window.addEventListener('load', () => {
+    twitterHideLoginDialogAddon.hide();
+});

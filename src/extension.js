b0e4ef0bf99a2b2526dde6ff7f25f64581b2e9b7
MTurnhout <MTurnhout@users.noreply.github.com>
2022-06-06 23:01:34 +0200

    Fix issue going from home to profile. It would still display the login dialog.

--- src/extension.js
+++ src/extension.js
@@ -2,95 +2,48 @@
  * Twitter hide login dialog add-on functionality.
  */
 class TwitterHideLoginDialogAddon {
+    /** @type {RegExp} */
     static #loginUrlRegEx = /^\/(?:i\/|login).*/;
-    #observerLayers;
-    #observerParent;
-    #layersElement;
+
+    /** @type {MutationObserver} */
+    #observerHtmlStyle;
 
     /**
      * Hide login dialog on appropriate pages.
      */
-    hide() {
-        this.#checkUntilLayersExist();
-    }
+    start() {
+        this.#observerHtmlStyle = new MutationObserver(() => {
+            if (document.documentElement.style.overflow !== "auto") {
+                if (TwitterHideLoginDialogAddon.#loginUrlRegEx.test(window.location.pathname))
+                    return;
 
-    /**
-     * Wait till React application is done loading and
-     * start observing layers elements and parent.
-     */
-    #checkUntilLayersExist() {
-        const checkExistInterval = setInterval(() => {
-            this.#layersElement = document.getElementById("layers");
-            if (this.#layersElement) {
-                clearInterval(checkExistInterval);
-
-                this.#startObservingLayersAndParent();
-            }
-        }, 100);
-    }
-    
-    /**
-     * Start observing layers elements and parent.
-     */
-    #startObservingLayersAndParent() {
-        if (!this.#observerLayers) {
-            this.#observerLayers = new MutationObserver((mutations) => {
-                this.#onLayersMutations(mutations);
-            });
-        }
-        this.#observerLayers.observe(this.#layersElement, { childList: true });
-        
-        if (!this.#observerParent) {
-            this.#observerParent = new MutationObserver((mutations) => {
-                this.#onLayersParentMutations(mutations);
-            });
-            this.#observerParent.observe(this.#layersElement.parentNode, { childList: true });
-        }
-    }
-
-    /**
-     * Monitor if login dialog is added and hide on appropriate pages.
-     * @param {MutationRecord[]} mutations
-     */
-    #onLayersMutations(mutations) {
-        if (TwitterHideLoginDialogAddon.#loginUrlRegEx.test(window.location.pathname))
-            return;
-
-        for (const mutation of mutations) {
-            for (const addedNode of mutation.addedNodes) {
-                const isLoginDialog = document.evaluate(
-                    ".//div[@role='dialog']//span[text()='Log in']",
-                    addedNode,
-                    null,
-                    XPathResult.BOOLEAN_TYPE,
-                    null).booleanValue;
-            
-                if (isLoginDialog) {
-                    addedNode.style.display = "none";
-                    
-                    document.documentElement.style.overflow = "auto";
+                const layerElement = document.getElementById("layers");
+                if (layerElement) {
+                    const loginButtonElement = document.evaluate(
+                        ".//div[@role='dialog']//span[text()='Log in']",
+                        layerElement,
+                        null,
+                        XPathResult.ANY_UNORDERED_NODE_TYPE,
+                        null).singleNodeValue;
+                
+                    if (loginButtonElement) {
+                        const loginLayerELement = loginButtonElement.closest("#layers > div");
+                        if (loginLayerELement.style.display !== "none") {
+                            loginLayerELement.style.display = "none";
+                        
+                            document.documentElement.style.overflow = "auto";
+                        }
+                    }
                 }
             }
-        }
-    }
-    
-    /**
-     * Monitor if layers element is removed and observe new layers element.
-     * @param {MutationRecord[]} mutations
-     */
-    #onLayersParentMutations(mutations) {
-        for (const mutation of mutations) {
-            for (const removedNode of mutation.removedNodes) {
-                if (removedNode === this.#layersElement) {
-                    this.#observerLayers.disconnect();
-                    this.#checkUntilLayersExist();
-                }
-            }
-        }
+        });
+        this.#observerHtmlStyle.observe(
+            document.documentElement,
+            { attributes: true, attributeFilter: ["style"] });
     }
 }
 
 const twitterHideLoginDialogAddon = new TwitterHideLoginDialogAddon();
 window.addEventListener('load', () => {
-    twitterHideLoginDialogAddon.hide();
+    twitterHideLoginDialogAddon.start();
 });

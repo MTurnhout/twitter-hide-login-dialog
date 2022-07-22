c05cd08eec2541e62f1236e1a3425e2f0c224cfa
MTurnhout <MTurnhout@users.noreply.github.com>
2022-07-22 23:42:19 +0200

    Remove DOMContentLoaded in deferred script

--- src/options.js
+++ src/options.js
@@ -1,20 +1,18 @@
 import { TwitterHideLoginDialogAddonOptions } from "./js/TwitterHideLoginDialogAddonOptions.js";
 
-document.addEventListener("DOMContentLoaded", () => {
-  const options = new TwitterHideLoginDialogAddonOptions();
+const options = new TwitterHideLoginDialogAddonOptions();
 
-  options.restoreOptions();
-  options.setLocalizedText();
+options.restoreOptions();
+options.setLocalizedText();
 
-  document.getElementById("saveButton").addEventListener("click", () => {
-    options.saveOptions();
-  });
-
-  document
-    .getElementById("loginButtonTextInput")
-    .addEventListener("keydown", (event) => {
-      if (event.key === "Enter") {
-        options.saveOptions();
-      }
-    });
+document.getElementById("saveButton").addEventListener("click", () => {
+  options.saveOptions();
 });
+
+document
+  .getElementById("loginButtonTextInput")
+  .addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+      options.saveOptions();
+    }
+  });
